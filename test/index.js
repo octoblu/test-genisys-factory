463e5a3a3d2efc1b8abe8189435d0e73b05271e0
Tin Phan <tinphan@Tins-MacBook-Pro.local>
2016-12-22 15:02:35 -0700

    book the room and test if hue light has correct color

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
-process.env.NODE_ENV = 'test';
-
 var expect   = require("chai").expect;
 var jsonFile = require("jsonfile");
 var hue      = require("node-hue-api");
 var HueApi   = require("node-hue-api").HueApi;
 var request  = require("request");
-
+var MeshbluHttp = require ('meshblu-http');
+var MeshbluConfig = require ('meshblu-config');
+var _ = require('lodash');
 
 var api = null;
 var meeting_room = null;
@@ -15,46 +15,71 @@ var meshblu_trigger = "https://triggers.octoblu.com/v2/flows/"
 var meshblu_hue_integration_flow = "f0e859b8-17e2-4dee-87fe-e71f733cc1d0"
 var meshblu_hue_trigger = "9c516ba1-c251-11e6-8ac8-5588f11c4255"
 var hue_in_meeting_state = 0;   //21844 is green(available), 0 is red(in meeting)
-
+var meshblu = null;
+var response = null;
+var currentMeeting = null;
 
 var displayStatus = function(status) {
     console.log(JSON.stringify(status, null, 2));
 };
 
-describe("Ad hoc meeting test:", function(){``
-  beforeEach("Setting up the test:", function(){
+describe("Ad hoc meeting test:", function(){
+  this.timeout(20000)
+
+  before("Setting up the test:", function(){
     meeting_room = jsonFile.readFileSync(meeting_room_file);
     api = new HueApi(meeting_room.bridge_ip, username);
     roomId = meeting_room.roomId
-    //console.log("Using Config: \n" + JSON.stringify(meeting_room, null, 2));
+    var meshbluConfig = new MeshbluConfig().toJSON()
+    meshblu = new MeshbluHttp(meshbluConfig)
   });
 
-  /*
-  afterEach("close the bridge connection", function(done){
-    api.close(done)
+  before('trigger to book a meeting', function(done){
+    request.post("https://triggers.octoblu.com/v2/flows/f0e859b8-17e2-4dee-87fe-e71f733cc1d0/triggers/9c516ba1-c251-11e6-8ac8-5588f11c4255",
+    {
+      json: {"roomId": roomId}
+    },
+    function (error, theResponse)
+    {
+      if(error) return done(error)
+      response = theResponse
+      _.delay(done, 15000)
+    })
   })
-  */
 
-  it("When you start an Ad hoc meeting by pressing Hue tap swicth", function(done){
-    console.log("I am in set meeting")
-    request.post("https://triggers.octoblu.com/v2/flows/f0e859b8-17e2-4dee-87fe-e71f733cc1d0/triggers/9c516ba1-c251-11e6-8ac8-5588f11c4255", {
-      json: {"roomId": "64bf33c0-e1e8-443c-8e0b-f79cb0b81284"},
-    }, function (error, response, body) {
-      if (error) return done(error);
-      if (response.statusCode !== 201) return done(new Error('Unexpected status code: ' + response.statusCode))
-      console.log("error: " + error)
-      console.log("response.statusCode: " + response.statusCode)
-      console.log("response.statusText: " + response.statusText)
+  before('get the room data', function(done){
+    meshblu.device(roomId, function(error, device){
+      if(error) return done(error)
+      currentMeeting = device.genisys.currentMeeting
       done()
     })
   })
 
-  it("Then the color of the light should change to red", function(done){
+  it("Toogle the trigger", function(){
+    expect(response.statusCode).to.equal(201)
+  })
+
+
+  it("Checking if the light respond accordingly", function(done){
     api.lightStatus(meeting_room.light_id, function(error, result) {
       if (error) return done(error)
-      displayStatus(result)
-      expect(result.state.hue).to.equal(hue_in_meeting_state)
+      if (result.state.hue == 0){
+        var color = "red"
+      }
+      else {
+        color = "green"
+      }
+      if (currentMeeting == null) {
+        expect(result.state.hue).to.equal(21844)
+        console.log('The room is avaible and light is ' + color)
+      }
+      if (currentMeeting != null){
+        expect(result.state.hue).to.equal(0)
+        console.log('The room is booked and light is ' + color)
+      }
       done()
     });
   });
+
+
 });
