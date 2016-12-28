4d2245106c138fe0cd774b8cdd8758a2696cb88d
jasonwiram <jwiram@gmail.com>
2016-12-28 10:32:10 -0700

    Organization code per standards

--- test/index.js
+++ test/index.js
@@ -1,30 +1,30 @@
-var expect   = require("chai").expect;
-var jsonFile = require("jsonfile");
-var hue      = require("node-hue-api");
-var HueApi   = require("node-hue-api").HueApi;
-var request  = require("request");
-var MeshbluHttp = require ('meshblu-http');
+var _             = require('lodash');
+var expect        = require('chai').expect;
+var hue           = require('node-hue-api');
+var HueApi        = require('node-hue-api').HueApi;
+var jsonFile      = require('jsonfile');
 var MeshbluConfig = require ('meshblu-config');
-var _ = require('lodash');
-var path = require ('path');
+var MeshbluHttp   = require ('meshblu-http');
+var path          = require ('path');
+var request       = require('request');
 
 var api = null;
-var meeting_room = null;
-var meeting_room_file = "./config/room.json";
-var username = "newdeveloper";
+var currentMeeting = null;
 var hue_in_meeting_state = 0;   //21844 is green(available), 0 is red(in meeting)
+var meeting_room = null;
+var meeting_room_file = './config/room.json';
 var meshblu = null;
 var response = null;
-var currentMeeting = null;
+var username = 'newdeveloper';
 
 var displayStatus = function(status) {
     console.log(JSON.stringify(status, null, 2));
 };
 
-describe("Ad hoc meeting test:", function(){
+describe('Ad hoc meeting test:', function(){
   this.timeout(20000)
 
-  before("Setting up the test:", function(){
+  before('Setting up the test:', function(){
     meeting_room = jsonFile.readFileSync(meeting_room_file);
     api = new HueApi(meeting_room.bridge_ip, username);
     roomId = meeting_room.roomId
@@ -35,7 +35,7 @@ describe("Ad hoc meeting test:", function(){
   before('trigger to book a meeting', function(done){
     request.post(meeting_room.url,
     {
-      json: {"roomId": roomId}
+      json: {'roomId': roomId}
     },
     function (error, theResponse)
     {
@@ -53,19 +53,19 @@ describe("Ad hoc meeting test:", function(){
     })
   })
 
-  it("Toogle the trigger", function(){
+  it('Toogle the trigger', function(){
     expect(response.statusCode).to.equal(201)
   })
 
 
-  it("Checking if the light respond accordingly", function(done){
+  it('Checking if the light respond accordingly', function(done){
     api.lightStatus(meeting_room.light_id, function(error, result) {
       if (error) return done(error)
       if (result.state.hue == 0){
-        var color = "red"
+        var color = 'red'
       }
       else {
-        color = "green"
+        color = 'green'
       }
       if (currentMeeting == null) {
         expect(result.state.hue).to.equal(21844)
