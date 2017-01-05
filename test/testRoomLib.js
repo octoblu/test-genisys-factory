ee6f343106445b3e58ecadf4335d73e22cf526c1
Tin Phan <tinphan@Tins-MacBook-Pro-2.local>
2017-01-04 18:21:21 -0700

    Fix updates

--- test/testRoomLib.js
+++ test/testRoomLib.js
@@ -1,32 +1,6 @@
 var Room = require("../lib/room.js")
 var room = new Room("../config/room.json");
 
-room.isBooked(function(error, result){
-  if(error){
-    console.log("isBooked Error : " + error);
-  }
-  else {
-    console.log("isBooked result : " + result);
-  }
-});
-//var status = room.isLightInMeeting();
 
-/*room.isLightInMeeting(function(error, result){
-  if(error){
-    console.log("islIghtInMeeting Error : " + error);
-  }
-  else {
-    console.log("islIghtInMeeting result : " + result);
-  }
-});*/
 
-// room.triggerSwitch(function(error,result){
-//   if(error){
-//     console.log("bookRoom Error : " + error);
-//   }
-//   else {
-//     console.log("bookRoom result : " , result);
-//   }
-// }
-//
-// )
+room.isSkypeStarting()
