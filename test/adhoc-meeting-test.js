8d6bdd3b294d8b07c42238f3f931fc95db57eea7
amir.mukeri / tinphan001 <amir.mukeri@citrix.com , tin.phan@citrix.com>
2017-01-10 11:58:09 -0700

    Update roomState and lightColor

--- test/adhoc-meeting-test.js
+++ test/adhoc-meeting-test.js
@@ -17,21 +17,46 @@ var state = {}
 
 
 describe('Create Ad hoc meeting test:', function()  {
-    this.timeout(15000)
+    this.timeout(20000)
+    let roomState = {}
     before('Create Room test ', function(done) {
-      done()
+      room.createAdHocMeeting(function(error) {
+        if(error){
+          console.log("Error createAdHocMeeting : " + error.message);
+          done(error)
+        }
+        setTimeout(function(){
+          room.getRoomState(function(result){
+            roomState = result
+            console.log('the state :', roomState)
+            done()
+          })
+        }, 15000);
+      })
+    })
+//
+    it('verify that currentMeeting exists', function() {
+      expect(roomState.currentMeeting).to.exist
     })
 
-
-
-    it('verify that currentMeeting exists', function(done) {
-      setTimeout(function(){
-        room.getRoomState(function(result){
-          state = result
-          console.log('the state :', state)
-          done()
-        })
-      }, 7000);
+    it('verify that meetingUrl exists', function() {
+      expect(roomState.meetingUrl).to.exist
     })
+
+    it('verify that inSkype property is set to false', function() {
+      expect(roomState.inSkype).to.be.false
+    })
+
+    it('verify that the color of Hue light is Red', function(done) {
+      room.getLightColor(function(error, color) {
+        if(error) {
+          console.log('Error' + error.message);
+          done(error)
+        }
+        expect(color).to.equal('Red')
+        done()
+      })
+    })
+
 
 })
