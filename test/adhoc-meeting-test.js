7a5a185afaee488b545e9a2fc0a9810ba5b00ec5
amir.mukeri / tinphan001 <amir.mukeri@citrix.com , tin.phan@citrix.com>
2017-01-09 18:58:06 -0700

    Updated the testing framework

---
+++ test/adhoc-meeting-test.js
@@ -0,0 +1,32 @@
+'use strict'
+var path   = require('path')
+var expect = require('chai').expect;
+var assert = require('assert');
+
+// var btnCredFile = {filename: path.join(__dirname, '../config/skydyne/hue_button.json')}
+
+var btnCredFile   = require('../config/octobluconf/button.json')
+var roomOptions   = require('../config/octobluconf/room.json')
+var meshbluConfig = require('../config/meshblu-dev.json')
+var Room          = require('../lib/room.js')
+
+var room = new Room({ room: roomOptions, meshbluConfig: meshbluConfig, btnCredFile: btnCredFile });
+
+var state = {}
+
+
+
+describe('Create Ad hoc meeting test:', function()  {
+    before('Create Room test ', function(done) {
+      room.getRoomState(function(result){
+        state = result
+        console.log('the state :', state)
+        done()
+      })
+    })
+    it('verify that currentMeeting exists', function() {
+
+
+    })
+
+})
--- test/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var util = require('gulp-util');
-
-gulp.task('test', function () {
-    return gulp.src(['test/index.js'], { read: false })
-        .pipe(mocha({ reporter: 'spec' }))
-        .on('error', util.log);
-});
